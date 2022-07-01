6ac2f8dcd6681e66d8b5a7f5acd79fa650b12bbe
Litvinenka <letuwwa@gmail.com>
2022-07-01 19:00:58 +0400

    feat: base model

--- artefacts/models.py
+++ artefacts/models.py
@@ -1,10 +1,24 @@
+import uuid
 from django.db import models
 
 
-class Artefact(models.Model):
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Artefact(BaseModel):
     title = models.CharField(max_length=256)
     country = models.CharField(max_length=128, null=True)
     description = models.CharField(max_length=512, null=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "artefact"
+        verbose_name_plural = "artefacts"
